4a00ee48939ddc32a3a3c1a003e8508dfdf38f48
Alex Chaves <alecxandroch@gmail.com>
2022-03-05 23:06:41 -0300

    auth done

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 import express from "express";
 import { dbConnect } from "./db/connection.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(express.json());
-const PORT = 3000;
+const PORT = 4000;
+
+import userRoute from "./routes/userRoute.js";
 
 app.get("/", async (req, res) => {
   res.status(200).json({ msg: "API running..." });
 });
 
-app.post("/auth/register", async (req,res) => {
-    const {name, email, password, confirmPassword} = await req.body;
-    if (!name) res.status(422).json({msg: "name is mandatory!"})
-
-})
-
+app.use("/users", userRoute);
 
 await dbConnect();
+
+//Models
 
 app.listen(PORT, () => console.log(`working on port ${PORT}`));
--- db/credentials.js
+++ db/credentials.js
@@ -3,5 +3,6 @@ dotenv.config()
 
 export const credentials = {
     db_user: process.env.DB_USER,
-    db_pass: process.env.DB_PASS
+    db_pass: process.env.DB_PASS,
+    secret: process.env.SECRET
 }
